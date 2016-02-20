faca63704dcc8578998e68422a3297dd7df674f4
Prasanna Nadimpalli <iamthe007@gmail.com>
2016-02-20 14:02:50 -0700

    Update cachematrix.R
    Week 3 R programming assignment commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below use the concepts of lexical scoping in caching matrix
+## Inverses
 
-## Write a short comment describing this function
+## returns a list of functions that get/set a matrix and get/set inverse of a matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrixforinverse = matrix()) {
+       i <- NULL
+       set <- function(y) {
+              matrixforinverse <<- y
+              i <<- NULL
+       }
+       get <- function() matrixforinverse
+       setinverse <- function(solve) i <<- solve
+       getinverse <- function() i
+       list(set = set, get = get,
+            setinverse = setinverse,
+            getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## first checks if inverse of matrix exists in cache. if exists returns from cache else computes inverse of mqatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       inversematrix <- x$getinverse()
+       if(!is.null(inversematrix)) {
+              message("getting cached data")
+              return(inversematrix)
+       }
+       data <- x$get()
+       inversematrix <- solve(data, ...)
+       x$setinverse(inversematrix)
+       inversematrix
 }
